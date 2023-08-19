3e975e040a967d1b98e84d81e1d61207b1942de0
Berke Parıldar <94047726+berkeparildar@users.noreply.github.com>
2023-08-19 04:29:13 +0300

    added tunnel obstacle, added ducking animation and mechanism,  detection of collisions with an obstacle or high platform, fixed a couple animations

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
     public GameObject box;
+    public GameObject tunnel;
     private int _chanceToContinueHigh;
     private static bool _highPlatformSpawning;
     private static int _currentAxisOfHigh;
     private static int _numberOfHighPlatforms;
+    private static int _emptySpot;
     private static int _oldIndex;
     private static bool _initAfterContinuation;
     
@@ -27,6 +29,7 @@ public class Platform : MonoBehaviour
             if (chanceToBeHigh != 4) return;
             _highPlatformSpawning = true;
             _numberOfHighPlatforms = Random.Range(6, 10);
+            _emptySpot = Random.Range(3, _numberOfHighPlatforms);
             // 0 Left, 1 Middle, 2 Right
             _currentAxisOfHigh = Random.Range(0, 3);
         }
@@ -43,7 +46,15 @@ public class Platform : MonoBehaviour
                 spots.Remove(_currentAxisOfHigh - 1);
                 var coinToss = Random.Range(0, 2);
                 var boxXPos = spots[coinToss];
-                Instantiate(box, new Vector3(boxXPos, transform.position.y + 0.8f, transform.position.z), Quaternion.identity);
+                var boxOrTunnel = Random.Range(0, 2);
+                if (boxOrTunnel == 1)
+                {
+                    Instantiate(box, new Vector3(boxXPos, transform.position.y + 0.8f, transform.position.z), Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(tunnel, new Vector3(boxXPos, transform.position.y + 0.8f, transform.position.z), Quaternion.identity);
+                }
             }
             else
             {
@@ -58,16 +69,19 @@ public class Platform : MonoBehaviour
     {
         if (_highPlatformSpawning)
         {
-            var indexOfHighPlatform = _currentAxisOfHigh + 3;
-            transform.GetChild(_currentAxisOfHigh).gameObject.SetActive(false);  
-            transform.GetChild(indexOfHighPlatform).gameObject.SetActive(true);
-            if (_initAfterContinuation)
+            if (_numberOfHighPlatforms != _emptySpot)
             {
-                var index = _oldIndex + 3;
-                transform.GetChild(_oldIndex).gameObject.SetActive(false);  
-                transform.GetChild(index).gameObject.SetActive(true);
-                _initAfterContinuation = false;
-                Debug.Log("Should Initialize two");
+                var indexOfHighPlatform = _currentAxisOfHigh + 3;
+                transform.GetChild(_currentAxisOfHigh).gameObject.SetActive(false);  
+                transform.GetChild(indexOfHighPlatform).gameObject.SetActive(true);
+                if (_initAfterContinuation)
+                {
+                    var index = _oldIndex + 3;
+                    transform.GetChild(_oldIndex).gameObject.SetActive(false);  
+                    transform.GetChild(index).gameObject.SetActive(true);
+                    _initAfterContinuation = false;
+                    Debug.Log("Should Initialize two");
+                }
             }
             _numberOfHighPlatforms--;
             if (_numberOfHighPlatforms == 0)
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,18 +5,25 @@ public class Player : MonoBehaviour
 {
     private Rigidbody _rigidbody;
     private Animator _modelAnimator;
+    private BoxCollider _boxCollider;
     private float _speed;
     private float _targetX;
     private readonly float _smoothTime = 0.03f;
     private bool _hasObstacleLeft;
     private bool _hasObstacleRight;
+    private bool _isOnFloor;
     private static readonly int TurnRight = Animator.StringToHash("turnRight");
     private static readonly int TurnLeft = Animator.StringToHash("turnLeft");
+    private static readonly int Jump = Animator.StringToHash("jump");
+    private static readonly int IsGrounded = Animator.StringToHash("isGrounded");
+    private static readonly int Jumping = Animator.StringToHash("jumping");
+    private static readonly int Duck1 = Animator.StringToHash("duck");
 
     private void Start()
     {
         _speed = 3;
         _rigidbody = GetComponent<Rigidbody>();
+        _boxCollider = GetComponent<BoxCollider>();
         _modelAnimator = transform.GetChild(0).GetComponent<Animator>();
     }
 
@@ -39,6 +46,16 @@ public class Player : MonoBehaviour
         {
             _hasObstacleLeft = false;
         }
+        
+        /*if (Physics.Raycast(transform.position, Vector3.down, out var ray, 0.05f))
+        {
+            if (ray.transform.CompareTag("Platform"))
+            {
+                Debug.Log("true");
+                _modelAnimator.SetTrigger(IsGrounded);
+            }
+        }*/
+        _modelAnimator.SetBool(Jumping,!Physics.Raycast(transform.position, Vector3.down, 0.2f));
     }
 
     private void Update()
@@ -51,18 +68,37 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _rigidbody.AddForce(Vector3.up * 6, ForceMode.Impulse);
+            _modelAnimator.SetTrigger(Jump);
         }
-        else if (Input.GetKeyDown(KeyCode.A) && transform.position.x > -0.99f && !_hasObstacleLeft)
+        else if (Input.GetKeyDown(KeyCode.A) && transform.position.x > -0.99f)
         {
             _targetX -= 1;
             _modelAnimator.SetTrigger(TurnLeft);
         }
-        else if (Input.GetKeyDown(KeyCode.D) && transform.position.x < 0.99f && !_hasObstacleRight)
+        else if (Input.GetKeyDown(KeyCode.D) && transform.position.x < 0.99f)
         {
             _targetX += 1;
             _modelAnimator.SetTrigger(TurnRight);
         }
-        Debug.DrawRay(transform.position, Vector3.left, Color.red);
+        Debug.DrawRay(transform.position, Vector3.down * 0.05f, Color.red);
         Debug.DrawRay(transform.position, Vector3.right, Color.green);
+        //Duck();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            _speed = 0;
+        }
+    }
+
+    private void Duck()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _modelAnimator.SetTrigger(Duck1);
+            //_boxCollider.size = new Vector3(0.5f, 0.9f, 0.5f);
+        }
     }
 }
